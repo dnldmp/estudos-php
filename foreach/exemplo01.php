31d42e98922471f781f88b525c7a9578e7ddb66d
dnldmp <danilodmp@hotmail.com>
2017-11-24 23:09:59 -0200

    Adicionando exemplos

---
+++ foreach/exemplo01.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$meses = array(
+	"Janeiro", "Fevereiro", "Março",
+	"Abril", "Maio", "Junho", "Julho", 
+	"Agosto", "Setembro", "Outubro", "Dezembro"
+	);
+
+	foreach ($meses as $index => $mes) {
+
+		echo "<br> Indice: " .$index. "<br>";
+		echo "O mes é: " .$mes. "<br>";
+
+
+	}
+
+ ?>
---
+++ foreach/exemplo02.php
@@ -0,0 +1,23 @@
+<form>
+	
+	<input type="text" name="nome">
+	<input type="date" name="nascimento">
+	<input type="submit" value="OK">
+
+</form>
+<?php 
+
+	if (isset($_GET)) {
+
+		foreach ($_GET as $key => $value) {
+			
+			echo "nome do campo: " .$key. "<br>";
+
+			echo "valor do campo" .$value;
+
+			echo "<hr>";
+
+		}
+	}
+
+ ?>
