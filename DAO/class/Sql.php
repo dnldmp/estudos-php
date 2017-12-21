36d8779b22329e718bec4e23dad0906b530ee5f2
dnldmp <danilodomp@gmail.com>
2017-12-21 20:19:17 -0200

    Adicionando novos exemplos no DAO

--- DAO/class/Sql.php
+++ DAO/class/Sql.php
@@ -10,11 +10,11 @@
 
 		}
 
-		private function setParams($statment, $parameters = array()){
+		private function setParams($statement, $parameters = array()){
 
 			foreach ($parameters as $key => $value){
 
-				$this->setParam($key, $value);
+				$this->setParam($statement, $key, $value);
 
 			}			
 
@@ -22,7 +22,7 @@
 
 		private function setParam($statement, $key, $value){
 
-			$statment->bindParam($key, $value);
+			$statement->bindParam($key, $value);
 
 		}
 
