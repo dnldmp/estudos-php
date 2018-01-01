7423e7419b0136648ce994ed6351a31256cc24c8
dnldmp <danilodomp@gmail.com>
2018-01-01 14:07:49 -0200

    Usando try catch

---
+++ try/exemplo01.php
@@ -0,0 +1,19 @@
+<?php 
+
+try{
+
+	throw new Exception("Houve um erro.", 400);
+	
+
+} catch (Exception $e) {
+
+	echo json_encode(array(
+		"message"=>$e->getMessage(),
+		"line"=>$e->getLine(),
+		"file"=>$e->getFile(),
+		"code"=>$e->getCode()
+	));
+
+}
+
+ ?>
