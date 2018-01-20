73c23f74248cd758184161f79571e0e7c005c334
dnldmp <danilodomp@gmail.com>
2018-01-20 20:57:26 -0200

    utilizando o slimframework

--- website/index.php
+++ website/index.php
@@ -1,5 +1,20 @@
 <?php
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+
+    echo json_encode(array(
+        'data'=>date("Y-m-d H:i:s")
+    ));
+
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+$app->run();
 
 ?>
